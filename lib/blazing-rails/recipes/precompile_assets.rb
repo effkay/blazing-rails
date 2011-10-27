a3d50ee2224c0aa7f0063cba6ab0b8c19a1cada9
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-10-27 17:34:35 +0200

    implement blazing 0.2 recipe API

--- lib/blazing-rails/recipes/precompile_assets.rb
+++ lib/blazing-rails/recipes/precompile_assets.rb
@@ -2,7 +2,8 @@ require 'blazing/recipe'
 
 class Blazing::Recipe::PrecompileAssets < Blazing::Recipe
 
-  def run
+  def run(target_recipes = {})
+    super target_recipes
     info 'precompiling assets'
     system 'bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile'
   end
