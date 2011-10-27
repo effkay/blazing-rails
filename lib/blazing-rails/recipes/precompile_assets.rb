238d238342cb3879fa97fb998c84b01991ef4bae
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-10-27 22:18:18 +0200

    use new logging interface

--- lib/blazing-rails/recipes/precompile_assets.rb
+++ lib/blazing-rails/recipes/precompile_assets.rb
@@ -4,7 +4,8 @@ class Blazing::Recipe::PrecompileAssets < Blazing::Recipe
 
   def run(target_recipes = {})
     super target_recipes
-    info 'precompiling assets'
+
+    logger.info 'precompiling assets'
     system 'bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile'
   end
 
