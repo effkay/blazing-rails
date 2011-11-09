49df4f88bf2c42cac07e79f0cf7917cf51f31425
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-11-09 17:03:43 +0100

    fix errors due to logger changes in blazing

--- lib/blazing-rails/recipes/precompile_assets.rb
+++ lib/blazing-rails/recipes/precompile_assets.rb
@@ -5,7 +5,7 @@ class Blazing::Recipe::PrecompileAssets < Blazing::Recipe
   def run(target_recipes = {})
     super target_recipes
 
-    logger.info 'precompiling assets'
+    info 'precompiling assets'
     system 'bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile'
   end
 
