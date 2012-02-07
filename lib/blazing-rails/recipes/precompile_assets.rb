53bdd9bc50d7997981f77dceedfc49fa9b867507
Felipe Kaufmann <felipekaufmann@gmail.com>
2012-02-07 21:24:31 +0100

    better naming of argument

--- lib/blazing-rails/recipes/precompile_assets.rb
+++ lib/blazing-rails/recipes/precompile_assets.rb
@@ -2,8 +2,8 @@ require 'blazing/recipe'
 
 class Blazing::Recipe::PrecompileAssets < Blazing::Recipe
 
-  def run(target_recipes = {})
-    super target_recipes
+  def run(target_options = {})
+    super target_options
 
     info 'precompiling assets'
     system 'bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile'
